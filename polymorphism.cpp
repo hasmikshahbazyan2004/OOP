b5bec045ab1201c8054bf127a3997d3cacc3d702
hasmikshahbazyan2004 <hasmikshahbazyan2004@gmail.com>
2025-01-06 23:28:40 +0400

    some changes were made in the file

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,1 +1,43 @@
-This iis a new file
+#include <iostream>
+using namespace std;
+
+class Animal{
+public:
+    void makeSound(){
+        cout << "Animal sound\n";
+    }    
+};
+
+class Dog: public Animal {
+public:
+    void makeSound() {
+        cout << "Woof woof\n";
+    }
+};
+
+class Cat: public Animal{
+public:
+    void makeSound() {
+        cout << "Meow\n";
+    }
+};
+
+int main(){
+    Animal* a1 = new Dog();
+    Animal* a2 = new Cat();
+
+    a1 -> makeSound(); //Output: Woof woof
+    a2 -> makeSound(); // Output: Meow
+
+    delete a1;
+    delete a2;
+}
+
+/* If we remove virtual from the base class Animal, the program will output "Animal"
+    for both calls. The reason is that when we watch to the derived object
+    through base pointer, we can just see base part of the object. But when we have 
+    virtual function in the base class and derived classes override it, the functions
+    from derived classes will be called.
+    THIS IS CALLED RUNTIME/DYNAMIC POLYMORPHISM.
+
+ */
